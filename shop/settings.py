09434dbef179cf11bf99aaac9fd53df19065b061
AlexLeyba <deathnotka@gmail.com>
2018-12-23 22:02:00 +0300

    Final 1.3

--- shop/settings.py
+++ shop/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'knmw*ivfkzty4eqnsvz2^v^j!56k4edbb^f)i$$s-)fo@($&s('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
